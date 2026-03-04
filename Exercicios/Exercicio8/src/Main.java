9077a926474ad8e359aaa5b4680d16cf49228ef6
Thiago <163765741+thbbsa@users.noreply.github.com>
2026-03-04 09:37:57 -0300

    Update Main.java

--- Exercicios/Exercicio8/src/Main.java
+++ Exercicios/Exercicio8/src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
     public static void main(String[] args) {
         boolean expressao1 = 120 - 30 == Math.pow(3, 30);
-        boolean expressao2 = !((20 % 4) == 1) || (9 != 9);
+        boolean expressao2 = !(((20 % 4) / 2) == 1) || (9 != 9);
         boolean expressao3 = (5 % 2) > 3;
 
         char a = 'a';
